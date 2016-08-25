0e46775301b47f40c3a82acf0a26c75855e07fb2
Sergio Mendoza <smendoza787@gmail.com>
2016-08-25 00:47:24 -0700

    removed contact page, added bootstrap to sign in, log in, and forgot password pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
 	get 'about' => 'pages#about'
-	get 'contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
