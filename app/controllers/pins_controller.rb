534d383eefaa9eb40dd2232d9331ca8b5e06877c
Sergio Mendoza <smendoza787@gmail.com>
2016-08-27 00:44:01 -0700

    Cleaned up the pins scaffold

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -17,35 +17,24 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
-
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render :show, status: :created, location: @pin }
-      else
-        format.html { render :new }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render :new
     end
   end
   
   def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pin }
-      else
-        format.html { render :edit }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to pins_url, notice: 'Pin was successfully destroyed.'
     end
   end
 
@@ -59,4 +48,3 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:description)
     end
-end
